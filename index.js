b9808ac01c8947f64dbc87e61660f2a186ce31d6
Quy <quynx1698@gmail.com>
2020-03-08 09:27:27 +0700

    Add movie list by text

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/movies", (req, res) =>
+  res.render("movies/index", {
+    movies: [
+      { id: 1, name: "Spider man" },
+      { id: 2, name: "Hulk" }
+    ]
+  })
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

7c8a217bd3a88d6ecf18ef3c3bb0a4664443a0bc
Quy <quynx1698@gmail.com>
2020-03-08 09:54:02 +0700

    Add search method

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const port = 3000;
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+let movies = [
+  { id: 1, name: "Spider man" },
+  { id: 2, name: "Hulk" }
+];
+
 app.get("/", (req, res) => res.render("index"));
 
 app.get("/movies", (req, res) =>
   res.render("movies/index", {
-    movies: [
-      { id: 1, name: "Spider man" },
-      { id: 2, name: "Hulk" }
-    ]
+    movies: movies
   })
 );
+
+app.get("/movies/search", (req, res) => {
+  const q = req.query.q;
+  let matchedMovies = movies.filter(
+    user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+  );
+
+  res.render("movies/index", {
+    movies: matchedMovies
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

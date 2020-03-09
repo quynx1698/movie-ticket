d4b0ec27d6aed4f0fe30ddd9db959ca31e9e9d4d
Quy <quynx1698@gmail.com>
2020-03-09 10:37:38 +0700

    Add lowdb database

--- index.js
+++ index.js
@@ -4,28 +4,33 @@ const port = 3000;
 
 const bodyParser = require("body-parser");
 
+const shortid = require("shortid");
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ movies: [] }).write();
+
 app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-let movies = [
-  { id: 1, name: "Spider man" },
-  { id: 2, name: "Iron man" },
-  { id: 3, name: "Hulk" }
-];
-
 app.get("/", (req, res) => res.render("index"));
 
 app.get("/movies", (req, res) =>
   res.render("movies/index", {
-    movies: movies
+    movies: db.get("movies").value()
   })
 );
 
 app.get("/movies/search", (req, res) => {
-  const q = req.query.q;
+  let q = req.query.q;
   let matchedMovies = movies.filter(
     user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
   );
@@ -40,8 +45,24 @@ app.get("/movies/create", (req, res) => {
 });
 
 app.post("/movies/create", (req, res) => {
-  movies.push(req.body);
+  req.body.id = shortid.generate();
+  db.get("movies")
+    .push(req.body)
+    .write();
   res.redirect("/movies");
+});
+
+app.get("/movies/:id", (req, res) => {
+  let id = req.params.id;
+
+  var movie = db
+    .get("movies")
+    .find({ id: id })
+    .value();
+
+  res.render("movies/view", {
+    movie: movie
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

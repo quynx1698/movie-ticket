1c0ca663328f29c0ee5328a74720163af465c189
Quy Nguyen <quynx1698@gmail.com>
2020-10-01 10:00:29 +0700

    Change port var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
